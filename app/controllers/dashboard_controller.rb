09c060f8fe49d751953bf196baacbccb96ef4508
Szabolcs Bokonyi <szabcsee@gmail.com>
2014-05-12 11:40:57 +0200

    Proper implementation of highcharts data preparation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,126 +13,19 @@ class DashboardController < ApplicationController
     data = {}
   	@payments.each do |payment|
       if payment.month.to_date.year.to_s === year.to_s
+        index = payment.month.to_date.month.to_i - 1
         case payment.payment_type
-          when "member"
-            case payment.month.to_date.month.to_s
-              when "1"
-                member_data[0] += payment.amount.to_i.round
-                payments_data[0] += 1
-              when "2"
-                member_data[1] += payment.amount.to_i.round
-                payments_data[1] += 1
-              when "3"
-                member_data[2] += payment.amount.to_i.round
-                payments_data[2] += 1
-              when "4"
-                member_data[3] += payment.amount.to_i.round
-                payments_data[3] += 1
-              when "5"
-                member_data[4] += payment.amount.to_i.round
-                payments_data[4] += 1
-              when "6"
-                member_data[5] += payment.amount.to_i.round
-                payments_data[5] += 1
-              when "7"
-                member_data[6] += payment.amount.to_i.round
-                payments_data[6] += 1
-              when "8"
-                member_data[7] += payment.amount.to_i.round
-                payments_data[7] += 1
-              when "9"
-                member_data[8] += payment.amount.to_i.round
-                payments_data[8] += 1
-              when "10"
-                member_data[9] += payment.amount.to_i.round
-                payments_data[9] += 1
-              when "11"
-                member_data[10] += payment.amount.to_i.round
-                payments_data[10] += 1
-              when "12"
-                member_data[11] += payment.amount.to_i.round
-                payments_data[11] += 1
-            end
-          when "supporter"
-            case payment.month.to_date.month.to_s
-              when "1"
-                supporter_data[0] += payment.amount.to_i.round
-                payments_data[0] += 1
-              when "2"
-                supporter_data[1] += payment.amount.to_i.round
-                payments_data[1] += 1
-              when "3"
-                supporter_data[2] += payment.amount.to_i.round
-                payments_data[2] += 1
-              when "4"
-                supporter_data[3] += payment.amount.to_i.round
-                payments_data[3] += 1
-              when "5"
-                supporter_data[4] += payment.amount.to_i.round
-                payments_data[4] += 1
-              when "6"
-                supporter_data[5] += payment.amount.to_i.round
-                payments_data[5] += 1
-              when "7"
-                supporter_data[6] += payment.amount.to_i.round
-                payments_data[6] += 1
-              when "8"
-                supporter_data[7] += payment.amount.to_i.round
-                payments_data[7] += 1
-              when "9"
-                supporter_data[8] += payment.amount.to_i.round
-                payments_data[8] += 1
-              when "10"
-                supporter_data[9] += payment.amount.to_i.round
-                payments_data[9] += 1
-              when "11"
-                supporter_data[10] += payment.amount.to_i.round
-                payments_data[10] += 1
-              when "12"
-                supporter_data[11] += payment.amount.to_i.round
-                payments_data[11] += 1
-            end
-          when "project"
-            case payment.month.to_date.month.to_s
-              when "1"
-                project_data[0] += payment.amount.to_i.round
-                payments_data[0] += 1
-              when "2"
-                project_data[1] += payment.amount.to_i.round
-                payments_data[1] += 1
-              when "3"
-                project_data[2] += payment.amount.to_i.round
-                payments_data[2] += 1
-              when "4"
-                project_data[3] += payment.amount.to_i.round
-                payments_data[3] += 1
-              when "5"
-                project_data[4] += payment.amount.to_i.round
-                payments_data[4] += 1
-              when "6"
-                project_data[5] += payment.amount.to_i.round
-                payments_data[5] += 1
-              when "7"
-                project_data[6] += payment.amount.to_i.round
-                payments_data[6] += 1
-              when "8"
-                project_data[7] += payment.amount.to_i.round
-                payments_data[7] += 1
-              when "9"
-                project_data[8] += payment.amount.to_i.round
-                payments_data[8] += 1
-              when "10"
-                project_data[9] += payment.amount.to_i.round
-                payments_data[9] += 1
-              when "11"
-                project_data[10] += payment.amount.to_i.round
-                payments_data[10] += 1
-              when "12"
-                project_data[11] += payment.amount.to_i.round
-                payments_data[11] += 1
-            end
-          end
+            when "member"
+                member_data[index] += payment.amount.to_i.round
+                payments_data[index] += 1
+            when "supporter"
+                supporter_data[index] += payment.amount.to_i.round
+                payments_data[index] += 1
+            when "project"
+                project_data[index] += payment.amount.to_i.round
+                payments_data[index] += 1
         end
+      end
     end
     data["member_data"] = member_data
     data["supporter_data"] = supporter_data
